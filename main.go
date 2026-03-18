326d3e7469139a8bdf9dc5a51afe0e3819f7ddbd
salonjee <salonbla338@gmail.com>
2026-03-18 21:50:35 +0545

    removed the trial phase docs

--- main.go
+++ main.go
@@ -1,73 +1,12 @@
 package main
 
 import (
-	//"bufio"
-	"errors"
-	"fmt"
-	"os"
-	"strings"
-	// "path/filepath"
+	
+	"ranger.com/exists"
 )
 
-
-type DataBox struct{
-	Image string `json:img`
-	Video string `json:mov`
-	Text string   `json:txt`
-
-}
-
-func checkExtensionExists(DirData string,Extension string)bool {
-	isit:=strings.Contains(DirData,Extension)
-	return isit
-}
-
-func checkFileExists(filePath string) bool {
-	_, error := os.Stat(filePath)
-	//return !os.IsNotExist(err)
-	return !errors.Is(error, os.ErrNotExist)
-}
-
-
 func main(){
-
-	Dir,err :=os.ReadDir(".")
-	if(err !=nil){
-	fmt.Println("error occurered : ",err)
-	}
-
-//for _,file:=range Dir {
-	//fmt.Printf("the file is %s ",file.Name())
-//}
-//files[]:=file.Name()
-///if(file.Name()=="image.png"){
-
-//}
-
-var files[] string
-for _,file:=range Dir{
-	files =append(files,file.Name())
-//fmt.Print(files)
-
+	exists.Existence("./trialDir",".png")
+	exists.Existence("./trialDir",".md")
+	exists.Existence("./trialDir",".mp4")
 }
-
-fmt.Printf("%+v\n",files)
-
-filesStringed :=fmt.Sprint(files)
-
-
-fmt.Print(checkExtensionExists(filesStringed,".png"))
-
-
-if checkFileExists("images")!=true{
-
-   if checkExtensionExists(filesStringed,".png")==true{
-	print("yes, the folder doenst exists, and the file png also exists")
-
-}
-
-}
-
-}
-
-
