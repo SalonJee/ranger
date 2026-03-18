905b9ea2b02936f0014028f57e4459e95d614fd8
salonjee <salonbla338@gmail.com>
2026-03-18 22:36:01 +0545

    now takes argument,and creates the dir

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
-	
+	"os"
 	"ranger.com/exists"
 )
 
 func main(){
-	exists.Existence("./trialDir",".png")
-	exists.Existence("./trialDir",".md")
-	exists.Existence("./trialDir",".mp4")
+
+	extension :=[...]string{"png","/.png",".jpeg",".jpg","jpg","jpeg",".mp4","mp4"}
+	args :=os.Args
+	if len(args) > 1 { //since args[0] is the main program name, we start at args[1]
+    targetDir := args[1]
+	for _, ext:=range extension{
+  exists.Existence(targetDir,ext)
+	}
+	
+	
+}
+
 }
